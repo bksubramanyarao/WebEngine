d86d764cf74b68ab58e53527666f11a2a62fd0b1
paooolino <paooolino@gmail.com>
2017-07-31 23:21:46 +0200

    first commit

---
+++ src/Machine.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The Machine
+ * 
+ * PHP version 5
+ *
+ * @category  Core
+ * @package   Machine
+ * @author    Paolo Savoldi <paooolino@gmail.com>
+ * @copyright 2017 Paolo Savoldi
+ * @license   https://github.com/paooolino/Machine/blob/master/LICENSE 
+ *            (Apache License 2.0)
+ * @link      https://github.com/paooolino/Machine    
+ */
+namespace Paooolino;
+
+/**
+ * Machine
+ * 
+ * Create a web application instantiating this class.
+ *
+ * @category Core
+ * @package  Machine
+ * @author   Paolo Savoldi <paooolino@gmail.com>
+ * @license  https://github.com/paooolino/Machine/blob/master/LICENSE 
+ *           (Apache License 2.0)
+ * @link     https://github.com/paooolino/Machine 
+ */
+class Machine
+{
+}
